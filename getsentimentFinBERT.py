064dc7b1e4446e2a9f7a3a70d83f1d1e2b15192b
Robert Grasnick <Robert Grasnick>
2026-01-17 21:13:46 +0100

    enable CUDA acceleration

--- getsentimentFinBERT.py
+++ getsentimentFinBERT.py
@@ -3,16 +3,17 @@ import torch
 import numpy as np
 import mariadb
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
 model_name = "ProsusAI/finbert"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name).to(device)
 config = AutoConfig.from_pretrained(model_name)
 
 id2label = config.id2label
 label2id = {v: k for k, v in id2label.items()}
 
 def get_finbert_score(text:str) -> float:
-    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=512)
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=512).to(device)
     with torch.no_grad():
         logits = model(**inputs).logits
     probs = torch.softmax(logits, dim=1).numpy()[0]
